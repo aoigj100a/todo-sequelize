88cc490cba3d3c1af18dc6d52e285592fa80c39e
aoigj100a <aoigj100@gmail.com>
2021-03-27 17:11:26 +0800

    feat:加入註冊功能

--- app.js
+++ app.js
@@ -39,12 +39,38 @@ app.get('/users/register', (req, res) => {
 
 app.post('/users/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
-    User.create({ name, email, password })
-        .then(user => res.redirect('/'))
+    User.findOne({ where: { email } }).then(user => {
+        if (user) {
+            console.log('User already exists')
+            return res.render('register', {
+                name,
+                email,
+                password,
+                confirmPassword
+            })
+        }
+        return bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(password, salt))
+            .then(hash => User.create({
+                name,
+                email,
+                password: hash
+            }))
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+    })
 })
 
 app.get('/users/logout', (req, res) => {
     res.send('logout')
+})
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id
+    return Todo.findByPk(id)
+        .then(todo => res.render('detail', { todo: todo.toJSON() }))
+        .catch(error => console.log(error))
 })
 
 app.listen(PORT, () => {
