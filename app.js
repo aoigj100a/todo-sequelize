ebf1dbfe2722ec7d11b31f476de1ea762371dd5e
aoigj100a <aoigj100@gmail.com>
2021-03-27 17:17:58 +0800

    feat:新增登入

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const passport = require('passport')
+const usePassport = require('./config/passport')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -15,6 +18,14 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true
+}))
+
+usePassport(app)
+require('./config/passport')
 
 app.get('/', (req, res) => {
     return Todo.findAll({
@@ -29,9 +40,10 @@ app.get('/users/login', (req, res) => {
     res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-    res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
     res.render('register')
