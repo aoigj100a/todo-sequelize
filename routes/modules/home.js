d277aa649f9ac187d8a6cc3d759bdf8544fc9a53
aoigj100a <aoigj100@gmail.com>
2021-03-28 15:26:45 +0800

    feat:增加middleware與新增todo

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,21 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-    return Todo.findAll({
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
         raw: true,
-        nest: true
+        nest: true,
+        where: { UserId: req.user.id }
+      })
     })
-        .then((todos) => { return res.render('index', { todos: todos }) })
-        .catch((error) => { return res.status(422).json(error) })
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.error(error))
 })
 
 module.exports = router
