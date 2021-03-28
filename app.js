44b03727ad3e5add9a9aafd7a06911411f2b0876
aoigj100a <aoigj100@gmail.com>
2021-03-28 15:51:17 +0800

    feat:隨著登入登出狀態改變header

--- app.js
+++ app.js
@@ -24,8 +24,13 @@ app.use(session({
 usePassport(app)
 require('./config/passport')
 
-app.use(routes)
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    next()
+  })
 
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log(`App is running on http://localhost:${PORT}`)
